318f083eb3a4e2d9fc849037f4de4b6c3cc5e628
wyfinger <wyfinger@yahoo.com>
2020-02-20 01:07:21 +1000

    change version field

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from cx_Freeze import setup, Executable
+from time import gmtime, strftime
+import re
+import os
 import sys
 
 packages = ['shlex', 'os', 'lxml']
@@ -7,8 +10,20 @@ includefiles = ['doc.ico']
 excludes = []
 path = []
 
+# set compilation time as a version
+sp = os.path.join(os.path.dirname(os.path.realpath(__file__)), "sp.py")
+with open(sp, 'r') as file :
+  filedata = file.read()
+
+# Replace the target string
+filedata = re.sub(r"(__version__ = )('\d{4}-\d{2}-\d{2}')", r"\1'"+strftime("%Y-%m-%d")+"'", filedata)
+
+# Write the file out again
+with open(sp, 'w') as file:
+  file.write(filedata)
+
 setup(name='SiemensPie',
-      version='0.06',
+      version='2.0.2',
       description='Tool for compile Siemens SIPROTEC relay configuration (.xml and .xrio files) to readable Excel document',
       executables=[
           Executable(
